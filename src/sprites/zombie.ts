cbc826b010f70fb78c4ecd917895ce269eeddb2b
Kleyson Prado <kleyson@kleyson.com.br>
2016-10-23 01:07:39 -0300

    Walking and jumping

--- src/sprites/zombie.ts
+++ src/sprites/zombie.ts
@@ -5,6 +5,8 @@ export class Zombie extends Sprite {
   game: Game;
   status: string;
   iterator: number;
+  floor_point: number;
+  JUMP_SIZE: number = 500;
 
   constructor({ game, x, y, asset = 'idle_1', scale }) {
     super(game, x, y, asset);
@@ -12,14 +14,44 @@ export class Zombie extends Sprite {
     this.status = 'idle';
     this.scale.x = scale;
     this.scale.y = scale;
-    this.anchor.setTo(0.05,0.95);
     this.iterator = 1;
+    this.floor_point = y + 10;
+    this.configure();
     setInterval(()=> this.updateTexture(), 100);
   }
 
-  updateTexture(){
+update(){
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.A)){
+        this.status = 'attack';
+    } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        this.status = 'walk';
+        this.y = this.floor_point - (this.JUMP_SIZE * this.scale.x) < this.y ? this.y-10: this.y ;
+        this.body.bounce.y = 0.2;
+    } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        this.status = 'walk';
+        this.x+=1;
+    } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        this.status = 'walk_back';
+        this.x-=1;
+    } else {
+        this.status = 'idle';
+    }
+  }
+
+  private configure(){
+    this.anchor.setTo(0.05,0.95);    
+    this.game.physics.enable( [ this ], Phaser.Physics.ARCADE)
+    this.body.gravity.y = 200;
+    this.body.collideWorldBounds = true;
+  }
+
+  private updateTexture(){
     let texture = '';
     switch(this.status) {
+        case 'walk_back':
+            this.iterator = this.iterator == 1 ? 9 : --this.iterator;
+            texture = `walk_${this.iterator}`;
+            break;
         case 'walk':
             this.iterator = this.iterator > 9 ? 1 : ++this.iterator;
             texture = `walk_${this.iterator}`;
@@ -41,16 +73,4 @@ export class Zombie extends Sprite {
     this.loadTexture(texture);
   }
 
-  update(){
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.A)){
-        this.status = 'attack';
-    }
-    else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-        this.status = 'walk';
-    }
-    else
-    {
-        this.status = 'idle';
-    }
-  }
 }
--- src/states/game-state.ts
+++ src/states/game-state.ts
@@ -1,7 +1,8 @@
 import { State, Stage} from 'phaser'
 import { Zombie, Background, Floor, Underground} from '../sprites'
 
-export class GameState extends Phaser.State {
+export class GameState extends State {
+  // Recover a global var.: let coisa = <any>this.game;
   stage: Stage;
   background: Background;
   zombie: Zombie;
@@ -11,31 +12,44 @@ export class GameState extends Phaser.State {
   create () {
     this.windowScale = +localStorage.getItem('windowsScale');
     this.stage.backgroundColor = '#FFF333'
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.arcade.gravity.y = 0;
     
-    // Recover a global var.
-    // let coisa = <any>this.game;
-    // console.log(coisa.data);
+    this.addCounter();
+    this.addBackground();
+    this.addFloor();
+    this.addZombie();
 
-    // let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Taca-le pau...', {});
-    // banner.fontSize = 40;
-    // banner.fill = '#77BFA3';
-    // banner.anchor.setTo(0.5);
+    this.game.world.setBounds(0, 0, this.game.width , this.game.height - (this.TILES_SIZE * this.windowScale)*2+10);
+  }
 
+
+  private addCounter(){
+     let banner = this.add.text(this.game.height -30, this.game.height - 30, 'Taca-le pau...', {});
+     banner.fontSize = 40;
+     banner.fill = '#77BFA3';
+     banner.anchor.setTo(0.5);
+  }
+
+
+  private addBackground(){
     this.background = new Background({ game: this.game, x: 0, y: 0 });
     this.game.add.existing(this.background);
+  }
 
+  private addFloor(){
     for (let i = 0; i <= Math.ceil(this.background.width/(this.TILES_SIZE * this.windowScale)); i++) {
       let underground = new Underground({ game: this.game, x: i, y: this.game.world.bottom, scale: this.windowScale});
       this.game.add.existing(underground);
       
-       let floor = new Floor({ game: this.game,  x: i, y: this.game.world.bottom - underground.height,  scale: this.windowScale});
-       this.game.add.existing(floor);
-       
+      let floor = new Floor({ game: this.game,  x: i, y: this.game.world.bottom - underground.height,  scale: this.windowScale});
+      this.game.add.existing(floor);  
     }
+  }
 
+  private addZombie(){
     this.zombie = new Zombie({ game: this.game, x: 100, y: this.game.world.bottom - (this.TILES_SIZE * this.windowScale)*2, scale: this.windowScale});
     this.game.add.existing(this.zombie);
-
   }
 
 }
